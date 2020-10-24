4e2d7387ffb9f63612f9c28ba9c631dbbf48c372
Use your GitHub username <Enter your email address>
2020-10-24 17:38:51 -0600

    lesson6 - updated JS for windchill

--- lesson-6/js/windchill.js
+++ lesson-6/js/windchill.js
@@ -1,15 +1,16 @@
 // windchill factor calculation
-var temp = document.getElementById("currTemp").value;
-var speed = document.getElementById("windSpeed").value;
-var output = calcWindchill(temp, speed);
+var temp = document.getElementById("currTemp").innerText;
+var windSpeed = document.getElementById("windSpeed").innerText;
+var output;
+if (temp <= 50 && windSpeed > 3) {
+    output = calcWindchill(temp, windSpeed);
+} else {
+    output = "N/A";
+}
 document.getElementById("windchill").innerHTML = output;
 
-function calcWindchill(tempF, windSpeed) {
-    let f = 35.74 + (0.6215 * tempF) - (35.75 * (windSpeed ** 0.16)) + 0.4275 * tempF * (windSpeed ** 0.16);
-    
-    if ((tempF <= 50) && (windSpeed > 3)) {
-        return f;
-    } else {
-        return "N/A";
-    }
+
+function calcWindchill(temp, windSpeed) {
+    let f = Math.round(35.74 + (0.6215 * temp) - (35.75 * (windSpeed ** 0.16)) + 0.4275 * temp * (windSpeed ** 0.16));
+    return f + "&deg;F"
 }
