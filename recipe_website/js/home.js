11b75f47c18cc3a5d8bbc2bb63f1a8bfc9952e06
Use your GitHub username <Enter your email address>
2021-05-06 15:01:01 -0600

    update

---
+++ recipe_website/js/home.js
@@ -0,0 +1,17 @@
+// get current year for copyright year in footer
+let now = new Date();
+let year = now.getFullYear();
+document.getElementById("current-yr").innerHTML = year;
+
+//document.getElementsByClassName("open").innerHTML = "&#8855 Close";
+
+//toggling the menu
+
+
+
+/*const hamButton = document.querySelector(".toggle");
+const mainNav = document.querySelector(".navigation");
+
+hamButton.addEventListener("click",() => {mainNav.classList.toggle("responsive")}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainNav.classList.remove("responsive")}; */
