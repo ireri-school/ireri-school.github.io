3fb13c2c4c73dd4da64e5562d1147e73420a4cec
Use your GitHub username <Enter your email address>
2021-12-20 11:36:20 -0700

    update

---
+++ Python/Twinkle.py
@@ -0,0 +1,13 @@
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+# \n = new line
+# \n\t = new line + indent
+# \n\t\t = new line + indent + indent
+
+# Result:
+
+# Twinkle, twinkle, little star,
+#       How I wonder what you are!
+#               Up above the world do high,
+#               Like a diamond in the sky.
+# Twinkle, twinkle, little star,
+#       How I wonder what you are!     
